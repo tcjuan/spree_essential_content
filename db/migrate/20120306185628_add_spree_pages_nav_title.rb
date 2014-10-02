9d2a89ca9e426be0a2dc948c98000aed780dfa4b
TC Juan <tcjuan@localhost.localdomain>
2014-10-02 20:25:41 +0800

    update add spree pages fields

--- db/migrate/20120306185628_add_spree_pages_nav_title.rb
+++ db/migrate/20120306185628_add_spree_pages_nav_title.rb
@@ -2,20 +2,12 @@ class AddSpreePagesNavTitle < ActiveRecord::Migration
   def self.up
     add_column :spree_pages, :nav_title,        :string
     add_column :spree_pages, :path,             :string
-    add_column :spree_pages, :meta_description, :string  
-    add_column :spree_pages, :meta_keywords,    :string  
-    add_column :spree_pages, :position,         :integer , :default => 999  
     add_column :spree_pages, :accessible,       :boolean , :default => true
-    add_column :spree_pages, :visible,          :boolean , :default => true
   end
 
   def self.down
     remove_column :spree_pages, :nav_title       
-    remove_column :spree_pages, :path
-    remove_column :spree_pages, :meta_description  
-    remove_column :spree_pages, :meta_keywords  
-    remove_column :spree_pages, :position  
+    remove_column :spree_pages, :path 
     remove_column :spree_pages, :accessible
-    remove_column :spree_pages, :visible
   end
 end
